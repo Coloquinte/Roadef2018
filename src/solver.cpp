9f49a3923de5bd15c29e61c8f57a5e1fb76ef5ec
Gabriel Gouvine <gabriel.gouvine_GIT@m4x.org>
2018-09-23 19:32:20 +0200

    Select most efficient moves before delivery

--- src/solver.cpp
+++ src/solver.cpp
@@ -33,7 +33,7 @@ Solver::Solver(const Problem &problem, SolverParams params, vector<int> initial)
 
   // Shuffle everything
   initializers_.emplace_back(make_unique<Shuffle>(  1));
-  initializers_.emplace_back(make_unique<Shuffle>(  4));
+  //initializers_.emplace_back(make_unique<Shuffle>(  4));
   initializers_.emplace_back(make_unique<Shuffle>( 16));
   initializers_.emplace_back(make_unique<Shuffle>( 64));
   initializers_.emplace_back(make_unique<Shuffle>(128));
@@ -41,11 +41,11 @@ Solver::Solver(const Problem &problem, SolverParams params, vector<int> initial)
   // Shuffle a range
   moves_.emplace_back(make_unique<Shuffle>(1,  8));
   moves_.emplace_back(make_unique<Shuffle>(1, 16));
-  moves_.emplace_back(make_unique<Shuffle>(1, 32));
+  //moves_.emplace_back(make_unique<Shuffle>(1, 32));
   moves_.emplace_back(make_unique<Shuffle>(4,  8));
   moves_.emplace_back(make_unique<Shuffle>(4, 16));
   moves_.emplace_back(make_unique<Shuffle>(4, 32));
-  moves_.emplace_back(make_unique<Shuffle>(8,  8));
+  //moves_.emplace_back(make_unique<Shuffle>(8,  8));
   moves_.emplace_back(make_unique<Shuffle>(8, 16));
   moves_.emplace_back(make_unique<Shuffle>(8, 32));
 
@@ -54,27 +54,27 @@ Solver::Solver(const Problem &problem, SolverParams params, vector<int> initial)
   moves_.emplace_back(make_unique<ItemInsert>());
   moves_.emplace_back(make_unique<RowInsert>());
   moves_.emplace_back(make_unique<CutInsert>());
-  moves_.emplace_back(make_unique<PlateInsert>());
+  //moves_.emplace_back(make_unique<PlateInsert>());
 
   // Swaps
   moves_.emplace_back(make_unique<ItemSwap>());
   moves_.emplace_back(make_unique<RowSwap>());
   moves_.emplace_back(make_unique<CutSwap>());
-  moves_.emplace_back(make_unique<PlateSwap>());
+  //moves_.emplace_back(make_unique<PlateSwap>());
 
   // Local swaps
-  moves_.emplace_back(make_unique<AdjacentItemSwap>());
+  //moves_.emplace_back(make_unique<AdjacentItemSwap>());
   moves_.emplace_back(make_unique<AdjacentRowSwap>());
   moves_.emplace_back(make_unique<AdjacentCutSwap>());
-  moves_.emplace_back(make_unique<AdjacentPlateSwap>());
+  //moves_.emplace_back(make_unique<AdjacentPlateSwap>());
 
   // Reverse a range
-  moves_.emplace_back(make_unique<Mirror>(4));
+  //moves_.emplace_back(make_unique<Mirror>(4));
   moves_.emplace_back(make_unique<Mirror>(8));
   moves_.emplace_back(make_unique<Mirror>(16));
 
   // Swap two ranges
-  moves_.emplace_back(make_unique<RangeSwap>());
+  //moves_.emplace_back(make_unique<RangeSwap>());
 
   for (const unique_ptr<Move> &m : initializers_) m->solver_ = this;
   for (const unique_ptr<Move> &m : moves_) m->solver_ = this;
