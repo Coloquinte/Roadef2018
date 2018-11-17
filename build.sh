4214b4c8519f8cfd2b6cdfaa4dbd0e1115b02f25
Gabriel Gouvine <gabriel.gouvine_GIT@m4x.org>
2018-11-17 11:08:17 +0100

    Update the PGO build for older compilers

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-OPTIONS="-g -I include/ -lboost_program_options -Wl,--whole-archive -lpthread -Wl,--no-whole-archive"
-g++ src/* -Og -o gcut_dbg $OPTIONS
-g++ src/* -O2 -o gcut_pg -fprofile-generate -DNDEBUG $OPTIONS
+OPTIONS="-g -std=c++14 -I include/ -lboost_program_options -Wl,--whole-archive -lpthread -Wl,--no-whole-archive"
+g++ src/*.cpp -Og -o gcut_dbg $OPTIONS
+g++ src/*.cpp -O2 -o gcut_pg -fprofile-generate -DNDEBUG $OPTIONS
 ./gcut_pg --batch dataset/A14_batch.csv --defects dataset/A14_defects.csv -t 60 -v 0 -j 1
-g++ src/* -O3 -o gcut -fprofile-use -static -DNDEBUG $OPTIONS
+g++ src/*.cpp -O3 -o gcut -fprofile-use -static -DNDEBUG $OPTIONS
