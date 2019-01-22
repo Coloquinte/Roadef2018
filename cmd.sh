bc2a215da6e5a202e439ca43ea8a1e86c43ceb92
Gabriel Gouvine <gabriel.gouvine_GIT@m4x.org>
2019-01-22 22:52:41 +0100

    Testing code

--- cmd.sh
+++ cmd.sh
@@ -1,10 +1,7 @@
-for s in {1..10}
+for t in 2 4 8 16 32
 do
-  for t in 1 2 4 8 16 32
+  for i in $(cat test_list.txt)
   do
-    for i in $(cat test_list.txt)
-    do
-      ./release/gcut_opt -p dataset/$i -v2 -t 180 -s $s -j $t -o results_180_${t}/${i}_${s}_solution.csv | tee results_180_${t}/${i}_${s}.log
-    done
+    ./release/gcut_opt -p dataset/$i -v2 -t 180 -s 0 -j $t -o results_180s/${t}T/${i}_${s}_solution.csv | tee results_180s/${t}T/${i}_${s}.log
   done
 done
